db1cdfa242ca3b099ac9265042172bcd51d42641
Galina Savenko <galina@MacBook-Pro-Galina.local>
2024-05-01 20:56:40 +0300

    create Booking project.

---
+++ Booking/Booking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Booking
+//
+//  Created by Galina Savenko on 01.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
